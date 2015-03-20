9f69b2298a5961043f033d381ad5ea9e60d21ac3
1004w455 <1004w455@naver.com>
2015-03-20 15:21:03 +0900

    문서 가져오기 예제

--- googleSheets/src/googleSheets/MySpreadsheet.java
+++ googleSheets/src/googleSheets/MySpreadsheet.java
@@ -15,10 +15,14 @@ import com.google.gdata.util.ServiceException;
 public class MySpreadsheet {
 
   public static void main(String[] args) throws IOException, ServiceException {
+    // sheets();
+    names();
+  }
 
+  public static void sheets() throws IOException, ServiceException {
     // 먼저 서비스를 만들고 로그인 정보를 입력합니다.
     SpreadsheetService service = new SpreadsheetService("MySpreadsheetIntegration-v1");
-    service.setUserCredentials("2014ksg@adwitt.com", "비밀번호");
+    service.setUserCredentials("2014ksg@adwitt.com", "rkdtkdrb123");
 
     // Define the URL to request. This should never change.
     URL SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
@@ -45,8 +49,61 @@ public class MySpreadsheet {
         }
       }
     }
-
-
   }
+
+  public static void names() throws IOException, ServiceException {
+    // 먼저 서비스를 만들고 로그인 정보를 입력합니다.
+    SpreadsheetService service = new SpreadsheetService("MySpreadsheetIntegration-v1");
+    service.setUserCredentials("2014ksg@adwitt.com", "rkdtkdrb123");
+
+    // Define the URL to request. This should never change.
+    URL SPREADSHEET_FEED_URL = new URL("https://spreadsheets.google.com/feeds/spreadsheets/private/full");
+
+    // feed.getEntries() 를 통해 등록되어 있는 모든 SpreadSheet 의 Entry 를 List 로 반환합니다.
+    SpreadsheetFeed feed = service.getFeed(SPREADSHEET_FEED_URL, SpreadsheetFeed.class);
+
+    List<SpreadsheetEntry> spreadsheets = feed.getEntries();
+
+    int cnt = 0;
+    
+    // 전체적인 구조는 SpreadSheetEntry -> WorksheetEntry -> CellEntry 의 형태를 가지며, 동일한 방법에 의해 접근이 가능합니다.
+    for (SpreadsheetEntry entry : spreadsheets) {
+
+
+      if (entry.getTitle().getPlainText().equals("와이즈버즈 비상연락망_2015.02.10.xlsx")) {
+        System.out.println(entry.getTitle().getPlainText());
+        List<WorksheetEntry> worksheets = entry.getWorksheets();
+        for (WorksheetEntry worksheet : worksheets) {
+          if (worksheet.getTitle().getPlainText().equals("03.09")) {
+            //System.out.println("1> \t" + worksheet.getTitle().getPlainText());
+
+            URL cellFeedUrl = worksheet.getCellFeedUrl();
+            CellFeed cellFeed = service.getFeed(cellFeedUrl, CellFeed.class);
+            for (CellEntry cell : cellFeed.getEntries()) {
+              if (cell.getTitle().getPlainText().contains("B")) {
+                //System.out.println("2> " + cell.getTitle().getPlainText());
+                String shortId = cell.getId().substring(cell.getId().lastIndexOf('/') + 1);
+                System.out.println(cell.getCell().getValue());
+                cnt++;
+              } else {
+                continue;
+              }
+            }
+          } else {
+            continue;
+          }
+        }
+        break;
+
+      } else {
+        continue;
+      }
+
+
+
+    }
+    System.out.println(cnt+" 명");
+  }
+
 
 }
